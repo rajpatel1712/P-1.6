ffd7141d704cafd93d656218d5e76035c36ce0fa
PATEL RAJ <110958017+rajpatel1712@users.noreply.github.com>
2022-10-04 21:04:08 +0530

    Practical 6 Completed

---
+++ MainActivity.kt
@@ -0,0 +1,37 @@
+package com.example.pr6_20012011122
+
+import android.content.Intent
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.MediaController
+import androidx.core.view.WindowCompat
+import com.example.pr6_20012011122.databinding.ActivityMainBinding
+import java.net.URI
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        videoplayer()
+
+        binding.btnSwitch.setOnClickListener {
+            Intent(this, youtubeactivity::class.java).also {
+                startActivity(it)
+            }
+        }
+    }
+    private fun videoplayer(){
+        val mediaController= MediaController(this)
+        val uri : Uri = Uri.parse("android.resource://" + packageName + "/" + R.raw.thestoryoflight)
+        binding.myVideoview.setMediaController(mediaController)
+        mediaController.setAnchorView(binding.myVideoview)
+        binding.myVideoview.setVideoURI(uri)
+        binding.myVideoview.requestFocus()
+        binding.myVideoview.start()
+    }
+}
